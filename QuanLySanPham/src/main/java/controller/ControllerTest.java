56d8912a0da40233204f3e1003027bc5ec75d3ff
QuyenNguyen-Huu <quyennh.18.student@fit.tdc.edu.vn>
2025-05-08 10:10:34 +0700

    Test 2 CRUD

--- QuanLySanPham/src/main/java/controller/ControllerTest.java
+++ QuanLySanPham/src/main/java/controller/ControllerTest.java
@@ -2,6 +2,6 @@ package controller;
 
 public class ControllerTest {
 	public static void main(String[] args) {
-        System.out.println("Hello from feature branch!");
+        System.out.println("Hello from crud branch!");
     }
 }
